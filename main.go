e1d0f6a16a463779fbbff7ac44ddd4d3587ff2fa
Vladislav <vladislav.bordioug@gmail.com>
2024-11-10 16:57:08 +0300

    fix

--- main.go
+++ main.go
@@ -192,7 +192,7 @@ func payment(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Wata error", http.StatusBadRequest)
 			return
 		}
-		req.Header.Add("Authorization", "Bearer "+os.Getenv("wata_sbp_token"))
+		req.Header.Add("Authorization", os.Getenv("wata_sbp_token"))
 		req.Header.Add("Content-Type", "application/json")
 		resp, err := client.Do(req)
 		if err != nil {
