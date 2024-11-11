2813f87de464b2cba66f9572b14d8f918d1c18e4
Vladislav <vladislav.bordioug@gmail.com>
2024-11-12 00:22:36 +0300

    fix

--- main.go
+++ main.go
@@ -195,26 +195,26 @@ type DigisellerStatusAnswer struct {
 }
 
 type CryptomusWebhookRequestData struct {
-	Type                    string  `json:"type"`
-	Uuid                    string  `json:"uuid"`
-	OrderID                 string  `json:"order_id"`
-	Amount                  string  `json:"amount"`
-	PaymentAmount           string  `json:"payment_amount"`
-	PaymentAmountUSD        string  `json:"payment_amount_usd"`
-	MerchantAmount          string  `json:"merchant_amount"`
-	Commission              string  `json:"commission"`
-	IsFinal                 bool    `json:"is_final"`
-	Status                  string  `json:"status"`
-	From                    string  `json:"from"`
+	Type                    *string `json:"type"`
+	Uuid                    *string `json:"uuid"`
+	OrderID                 *string `json:"order_id"`
+	Amount                  *string `json:"amount"`
+	PaymentAmount           *string `json:"payment_amount"`
+	PaymentAmountUSD        *string `json:"payment_amount_usd"`
+	MerchantAmount          *string `json:"merchant_amount"`
+	Commission              *string `json:"commission"`
+	IsFinal                 *bool   `json:"is_final"`
+	Status                  *string `json:"status"`
+	From                    *string `json:"from"`
 	WalletAddressUUID       *string `json:"wallet_address_uuid"`
-	Network                 string  `json:"network"`
-	Currency                string  `json:"currency"`
-	PayerCurrency           string  `json:"payer_currency"`
-	PayerAmount             string  `json:"payer_amount"`
-	PayerAmountExchangeRate string  `json:"payer_amount_exchange_rate"`
+	Network                 *string `json:"network"`
+	Currency                *string `json:"currency"`
+	PayerCurrency           *string `json:"payer_currency"`
+	PayerAmount             *string `json:"payer_amount"`
+	PayerAmountExchangeRate *string `json:"payer_amount_exchange_rate"`
 	AdditionalData          *string `json:"additional_data"`
 	TransferID              *string `json:"transfer_id"`
-	Txid                    string  `json:"txid"`
+	Txid                    *string `json:"txid"`
 }
 
 type CryptomusWebhookRequestSignature struct {
@@ -478,14 +478,7 @@ func webhookcryptomus(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Incorrect webhook", http.StatusBadRequest)
 		return
 	}
-	str := strconv.Quote(string(body))
-	for i := 0; i < len(str); i += 100 {
-		end := i + 100
-		if end > len(str) {
-			end = len(str)
-		}
-		fmt.Println(str[i:end])
-	}
+	//str := strconv.Quote(string(body))
 	var respdata CryptomusWebhookRequestData
 	if err := json.Unmarshal(body, &respdata); err != nil {
 		http.Error(w, "Incorrect webhook", http.StatusBadRequest)
@@ -504,7 +497,7 @@ func webhookcryptomus(w http.ResponseWriter, r *http.Request) {
 	data = []byte(strings.Replace(string(data), `/`, `\/`, -1))
 	sign := md5hash(data)
 	if sign != signrespdata.Signature {
-		http.Error(w, "Incorrect IP", http.StatusBadRequest)
+		http.Error(w, "Incorrect signature", http.StatusBadRequest)
 		return
 	}
 	fmt.Println("success")
