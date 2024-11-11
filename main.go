aa9a48729e0ef687c9f014dfaca9dbaae5d27e37
Vladislav <vladislav.bordioug@gmail.com>
2024-11-11 13:28:43 +0300

    fix

--- main.go
+++ main.go
@@ -178,27 +178,27 @@ type CryptomusError struct {
 }
 
 type CryptomusWebhookRequestData struct {
-	Type              string  `json:"type"`
-	Uuid              string  `json:"uuid"`
-	OrderID           string  `json:"order_id"`
-	Amount            string  `json:"amount"`
-	PaymentAmount     string  `json:"payment_amount"`
-	PaymentAmountUSD  string  `json:"payment_amount_usd"`
-	MerchantAmount    string  `json:"merchant_amount"`
-	Commission        string  `json:"commission"`
-	IsFinal           bool    `json:"is_final"`
-	Status            string  `json:"status"`
-	From              string  `json:"from"`
-	WalletAddressUUID *string `json:"wallet_address_uuid"`
-	Network           string  `json:"network"`
-	Currency          string  `json:"currency"`
-	PayerCurrency     string  `json:"payer_currency"`
-	AdditionalData    *string `json:"additional_data"`
-	Convert           *struct {
-		ToCurrency string  `json:"to_currency"`
-		Commission *string `json:"commission"`
-		Rate       string  `json:"rate"`
-		Amount     string  `json:"amount"`
+	Type              string `json:"type"`
+	Uuid              string `json:"uuid"`
+	OrderID           string `json:"order_id"`
+	Amount            string `json:"amount"`
+	PaymentAmount     string `json:"payment_amount"`
+	PaymentAmountUSD  string `json:"payment_amount_usd"`
+	MerchantAmount    string `json:"merchant_amount"`
+	Commission        string `json:"commission"`
+	IsFinal           bool   `json:"is_final"`
+	Status            string `json:"status"`
+	From              string `json:"from"`
+	WalletAddressUUID string `json:"wallet_address_uuid"`
+	Network           string `json:"network"`
+	Currency          string `json:"currency"`
+	PayerCurrency     string `json:"payer_currency"`
+	AdditionalData    string `json:"additional_data"`
+	Convert           struct {
+		ToCurrency string `json:"to_currency"`
+		Commission string `json:"commission"`
+		Rate       string `json:"rate"`
+		Amount     string `json:"amount"`
 	} `json:"convert"`
 	Txid string `json:"txid"`
 }
@@ -470,14 +470,7 @@ func webhookcryptomus(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Println(respdata)
-	ip, _, err := net.SplitHostPort(r.RemoteAddr)
-	fmt.Println(r.RemoteAddr, ip)
-	if err != nil {
-		http.Error(w, "Unable to parse RemoteAddr", http.StatusBadRequest)
-		return
-	}
 	IPAddress := r.Header.Get("X-Forwarded-For")
-	fmt.Println(IPAddress)
 	if IPAddress != "91.227.144.54" {
 		http.Error(w, "Incorrect IP", http.StatusBadRequest)
 		return
